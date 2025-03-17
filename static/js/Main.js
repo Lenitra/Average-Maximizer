43f848ac92c3ed5775bbed56a540a69f07134872
Thomas L <lenitramc@gmail.com>
2025-03-17 09:20:50 +0100

    Archi sys

--- static/js/Main.js
+++
@@ -1,6 +0,0 @@
-import { App } from "./App.js";
-
-document.addEventListener("DOMContentLoaded", () => {
-  const appContainer = document.getElementById("app");
-  new App(appContainer);
-});
--- static/js/Note.js
+++
@@ -1,7 +0,0 @@
-export class Note {
-  constructor(value, coef, locked = false) {
-    this.value = value;
-    this.coef = coef;
-    this.locked = locked;
-  }
-}
