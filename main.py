d35ee1a8e02cfff79d60e3548a38f4b5ff616ae0
Thomas L <lenitramc@gmail.com>
2025-03-19 10:46:35 +0100

    Home page

--- main.py
+++ main.py
@@ -4,8 +4,12 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def calculator():
     return render_template('calculator.html')
+
+@app.route('/home')
+def home():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
